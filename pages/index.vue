27567532bd61da8fc30b305279b564afda90ac0f
Ghany Abdillah Ersa <ghanyersa24@gmail.com>
2022-10-02 11:52:24 +0700

    init project with setup airbnb style

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <tutorial>
+    <div class="class-meeting">
+      <h1>adadsa</h1>
+      <nuxt-logo />
+    </div>
+  </tutorial>
+</template>
+
+<script>
+import NuxtLogo from '~/components/NuxtLogo.vue';
+const caraBertag = 'testing yah';
+export default {
+  components: { NuxtLogo },
+  name: 'IndexPage',
+  data() {
+    return {
+      text: `ini cara ${caraBertag}`,
+    };
+  },
+};
+</script>
